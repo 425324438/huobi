80e166f7ed06bc49ebace49e72630ccda39f0880
shiyalong <425324438@qq.com>
2017-12-19 17:33:07 +0800

    update

---
+++ src/main/java/com/cn/huobi/redis/MyBean.java
@@ -0,0 +1,19 @@
+package com.cn.huobi.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 425324438@qq.com
+ * @date 2017/12/19 17:27
+ */
+@Component
+public class MyBean {
+    private StringRedisTemplate template;
+
+    @Autowired
+    public MyBean(StringRedisTemplate template) {
+        this.template = template;
+    }
+}
---
+++ src/test/java/com/cn/huobi/redis/RedisTest.java
@@ -0,0 +1,26 @@
+package com.cn.huobi.redis;
+
+import javafx.application.Application;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author 425324438@qq.com
+ * @date 2017/12/19 17:23
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class RedisTest {
+    @Autowired
+    private StringRedisTemplate template;
+
+    @Test
+    public void Test(){
+        template.opsForValue().get("username");
+    }
+}
